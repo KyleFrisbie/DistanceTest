f960652a2b7ea1c66f98a435c538198bcaada67b
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2015-10-18 15:25:04 -0600

    merge

--- app/src/main/java/com/kylefrisbie/distancetest/MainActivity.java
+++ app/src/main/java/com/kylefrisbie/distancetest/MainActivity.java
@@ -3,8 +3,6 @@ package com.kylefrisbie.distancetest;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
